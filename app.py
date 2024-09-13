f0cdc988f1f19263f4eec79491c0ad54a4e102a7
cst19041 <cst19041@std.uwu.ac.lk>
2024-09-13 08:14:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -108,4 +108,4 @@ def predict():
     return jsonify(response)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
